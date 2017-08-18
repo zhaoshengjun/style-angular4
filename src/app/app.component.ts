33a343e995f4a7bb760d3bb350846d262c47cba7
Sam Zhao <zhao.shengjun@gmail.com>
2017-08-18 13:29:11 +1000

    lesson7:hostbinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from "@angular/core";
-import { DomSanitizer } from "@angular/platform-browser";
+import { Component, HostBinding } from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -7,13 +6,5 @@ import { DomSanitizer } from "@angular/platform-browser";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  constructor(private sanitizer: DomSanitizer) {}
-
-  getStyle() {
-    const gravatarUrl =
-      "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50";
-    let style = `background-image:url(${gravatarUrl});width: 150px; height: 150px; border: 1px solid black`;
-    return this.sanitizer.bypassSecurityTrustStyle(style);
-    // return style;
-  }
+  @HostBinding("class.yellow-style") yellowStyle = true;
 }
